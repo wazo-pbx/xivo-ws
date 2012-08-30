3361ad9c275bd93d15e26303fd79b9f068a1eff1
Cedric Abunar <cabunar@avencall.com>
2012-08-30 15:07:51 -0400

    bump version

--- xivo_ws/version.py
+++ xivo_ws/version.py
@@ -15,4 +15,5 @@
 # You should have received a copy of the GNU General Public License
 # along with this program.  If not, see <http://www.gnu.org/licenses/>
 
-version = u'12.17.7'
+version = u'12.17.8'
+
