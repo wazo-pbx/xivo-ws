726dafb6613cd8fb75a4d9d8e9065e1ef14d9d2a
Sebastien Duthil <sduthil@avencall.com>
2012-10-23 17:23:13 -0400

    fix pep8 warnings

--- xivo_ws/objects/tests/test_schedule.py
+++ xivo_ws/objects/tests/test_schedule.py
@@ -23,129 +23,112 @@ from xivo_ws.objects.schedule import Schedule
 
 class TestSchedule(unittest.TestCase):
     def test_to_obj_dict(self):
-        expected_obj_dict = {'schedule': {
-                                'name': 'huit_a_midi',
-                                'timezone': 'America/Montreal',
-                                'description': ''
-                              },
-                             'dialaction': {
-                                 'schedule_fallback': {
-                                        "actiontype": 'endcall',
-                                        "action": 'hangup'
-                                    }
-                             },
-                             'opened': [
-                                        {'hours': '08:00-12:00',
-                                         'weekdays': '1-5',
-                                         'monthdays': '1-31',
-                                         'months': '1-12'
-                                        }
-                                    ],
-                             'closed': []
-                             }
-        opened = [
-                {
-                    "hours": "08:00-12:00",
-                    "weekdays": "1-5",
-                    "monthdays": "1-31",
-                    "months": "1-12",
+        expected_obj_dict = {
+            'schedule': {
+                'name': 'huit_a_midi',
+                'timezone': 'America/Montreal',
+                'description': ''
+            },
+            'dialaction': {
+                'schedule_fallback': {
+                    "actiontype": 'endcall',
+                    "action": 'hangup'
                 }
-            ]
+            },
+            'opened': [
+                {'hours': '08:00-12:00',
+                 'weekdays': '1-5',
+                 'monthdays': '1-31',
+                 'months': '1-12'}
+            ],
+            'closed': []
+        }
+        opened = [
+            {
+                "hours": "08:00-12:00",
+                "weekdays": "1-5",
+                "monthdays": "1-31",
+                "months": "1-12",
+            }
+        ]
         schedule = Schedule(name='huit_a_midi',
-                        timezone='America/Montreal',
-                        fallback_action='endcall:hangup',
-                        description='',
-                        opened=opened,
-                        closed=[])
+                            timezone='America/Montreal',
+                            fallback_action='endcall:hangup',
+                            description='',
+                            opened=opened,
+                            closed=[])
 
         obj_dict = schedule.to_obj_dict()
 
         self.assertEqual(expected_obj_dict, obj_dict)
 
     def test_to_obj_dict_with_no_dialaction(self):
-        expected_obj_dict = {'schedule': {
-                                'name': 'test',
-                                'timezone': 'America/Montreal',
-                                'description': ''
-                              },
-                             'dialaction': {
-                                 'schedule_fallback': {
-                                        "actiontype": 'none'
-                                    }
-                             },
-                             'opened': [
-                                        {'hours': '08:00-12:00',
-                                         'weekdays': '1-5',
-                                         'monthdays': '1-31',
-                                         'months': '1-12'
-                                        }
-                                    ],
-                             'closed': []
-                             }
-        opened = [
-                {
-                    "hours": "08:00-12:00",
-                    "weekdays": "1-5",
-                    "monthdays": "1-31",
-                    "months": "1-12",
+        expected_obj_dict = {
+            'schedule': {
+                'name': 'test',
+                'timezone': 'America/Montreal',
+                'description': ''
+            },
+            'dialaction': {
+                'schedule_fallback': {
+                    "actiontype": 'none'
                 }
-            ]
+            },
+            'opened': [
+                {'hours': '08:00-12:00',
+                 'weekdays': '1-5',
+                 'monthdays': '1-31',
+                 'months': '1-12'}
+            ],
+            'closed': []
+        }
+        opened = [
+            {
+                "hours": "08:00-12:00",
+                "weekdays": "1-5",
+                "monthdays": "1-31",
+                "months": "1-12",
+            }
+        ]
         schedule = Schedule(name='test',
-                        timezone='America/Montreal',
-                        fallback_action=None,
-                        description='',
-                        opened=opened,
-                        closed=[])
+                            timezone='America/Montreal',
+                            fallback_action=None,
+                            description='',
+                            opened=opened,
+                            closed=[])
 
         obj_dict = schedule.to_obj_dict()
 
         self.assertEqual(expected_obj_dict, obj_dict)
 
     def test_from_obj_dict(self):
-        obj_dict = {'schedule': {
-                        'id': '1',
-                        'name': 'huit_a_midi',
-                        'timezone': 'America/Montreal',
-                        'fallback_action': 'endcall:hangup',
-                        'fallback_actionid': None,
-                        'fallback_actionargs': '',
-                        'description': '',
-                        'commented': 0,
-                      },
-                     'dialaction': {
-                         'schedule_fallback': {
-                                "actiontype": None,
-                                "action": None,
-                                "endcall": {
-                                    "action": None,
-                                    "actionarg1": None,
-                                    "actionarg2": None
-                                },
-                            }
-                     },
-                     'opened': [
-                                {
-                                "id": 1,
-                                "schedule_id": 1,
-                                "mode": "opened",
-                                "hours": "08:00-12:00",
-                                "weekdays": "1-5",
-                                "monthdays": "1-31",
-                                "months": "1-12",
-                                "action": None,
-                                "actionid": None,
-                                "actionargs": None,
-                                "commented": False
-
-                                }
-                     ],
-                     'closed': False
-                     }
-        expected_opened = [
+        obj_dict = {
+            'schedule': {
+                'id': '1',
+                'name': 'huit_a_midi',
+                'timezone': 'America/Montreal',
+                'fallback_action': 'endcall:hangup',
+                'fallback_actionid': None,
+                'fallback_actionargs': '',
+                'description': '',
+                'commented': 0,
+            },
+            'dialaction': {
+                'schedule_fallback': {
+                    "actiontype": None,
+                    "action": None,
+                    "endcall": {
+                        "action": None,
+                        "actionarg1": None,
+                        "actionarg2": None
+                    },
+                }
+            },
+            'opened': [
                 {
                     "id": 1,
                     "schedule_id": 1,
-                    "mode": 'opened',
+                    "mode": "opened",
                     "hours": "08:00-12:00",
                     "weekdays": "1-5",
                     "monthdays": "1-31",
@@ -154,8 +137,26 @@ class TestSchedule(unittest.TestCase):
                     "actionid": None,
                     "actionargs": None,
                     "commented": False
+
                 }
-            ]
+            ],
+            'closed': False
+        }
+        expected_opened = [
+            {
+                "id": 1,
+                "schedule_id": 1,
+                "mode": 'opened',
+                "hours": "08:00-12:00",
+                "weekdays": "1-5",
+                "monthdays": "1-31",
+                "months": "1-12",
+                "action": None,
+                "actionid": None,
+                "actionargs": None,
+                "commented": False
+            }
+        ]
 
         schedule = Schedule.from_obj_dict(obj_dict)
 
