098d28c1f94031b88665412f063b98770d8135f6
Cedric Abunar <cabunar@avencall.com>
2014-01-23 07:27:13 -0500

    fix invalid path for README in setup.py

--- setup.py
+++ setup.py
@@ -18,7 +18,7 @@ setup(
               'xivo_ws.objects.common'],
     scripts=['bin/xivo-ws-debug'],
     license='GPLv3',
-    long_description=open('README').read(),
+    long_description=open('README.md').read(),
     classifiers=[
         'Development Status :: 4 - Beta',
         'Intended Audience :: Developers',
